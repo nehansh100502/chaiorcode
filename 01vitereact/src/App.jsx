8afda6c356e83adb46ebabad40a37ad02ecdbb32
nehansh100502 <nehansh100502@gmail.com>
2024-04-22 21:44:49 +0530

    react with hitesh sir

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+   <h1>CHAI AUR CODE with vite || neha singh </h1>
+       
+  )
+}
+export default App
---
+++ chaiaurcode/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>chai aur react || neha singh</h1>
+  );
+}
+
+export default App;
